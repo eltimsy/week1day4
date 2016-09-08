771cacb87707a9aa40d59e590454248f770c9f21
eltimsy <timothy.sc.tse@gmail.com>
2016-09-08 15:21:23 +0000

    added unique id

---
+++ unique-id.js
@@ -0,0 +1,12 @@
+var id = (function () {
+  count = -1
+  return function(){
+    count += 1
+    return count;
+  }
+})();
+
+console.log(id()); // 0
+console.log(id()); // 1
+console.log(id()); // 2
+console.log(id()); // 3
